875e460c9d1c426e278ae2388a760b07687cbbf8
ChiFAQ <46202807+ChiFAQ@users.noreply.github.com>
2020-03-24 16:24:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+import os
 
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
